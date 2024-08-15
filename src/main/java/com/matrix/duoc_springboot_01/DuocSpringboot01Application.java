d201c89a23ea7e5e7131d9986bbda2106b2436e8
Sebastián Kravetz <sebastiankravetz@icloud.com>
2024-08-14 23:51:13 -0400

    starter repo

---
+++ src/main/java/com/matrix/duoc_springboot_01/DuocSpringboot01Application.java
@@ -0,0 +1,13 @@
+package com.matrix.duoc_springboot_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuocSpringboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuocSpringboot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/matrix/duoc_springboot_01/DuocSpringboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.duoc_springboot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuocSpringboot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
