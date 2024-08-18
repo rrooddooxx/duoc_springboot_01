bfc62b85943c0f72404213eddd37dc06a1f07550
Sebastián Kravetz <sebastiankravetz@icloud.com>
2024-08-18 17:54:54 -0400

    .

--- src/main/java/com/matrix/duoc_springboot_01/infrastructure/controllers/MoviesController.java
+++ src/main/java/com/matrix/duoc_springboot_01/infrastructure/controllers/MoviesController.java
@@ -2,30 +2,36 @@ package com.matrix.duoc_springboot_01.infrastructure.controllers;
 
 import com.matrix.duoc_springboot_01.application.services.MoviesService;
 import com.matrix.duoc_springboot_01.domain.Movies;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class MoviesController {
 
-    private final MoviesService moviesService;
+  private final MoviesService moviesService;
 
-    @GetMapping("/movies")
-    public List<Movies> getAllMovies(@RequestParam(required = false) Integer limit) {
-        Optional<Integer> limitOptional = Optional.ofNullable(limit);
-        return limitOptional.isPresent() ? moviesService.getAllMovies(limit) : moviesService.getAllMovies();
-    }
+  @GetMapping("/movies")
+  public ResponseEntity<List<Movies>> getAllMovies(@RequestParam(required = false) Integer limit) {
+    Optional<Integer> limitOptional = Optional.ofNullable(limit);
+    List<Movies> serviceResponse =
+        limitOptional.isPresent()
+            ? moviesService.getAllMovies(limit)
+            : moviesService.getAllMovies();
+    return serviceResponse.isEmpty()
+        ? ResponseEntity.notFound().build()
+        : ResponseEntity.ok(serviceResponse);
+  }
 
-    @GetMapping("/movies/{movieId}")
-    public List<Movies> getMovieById(
-            @PathVariable int movieId
-    ) {
-        return moviesService.getMovieById(movieId);
-    }
+  @GetMapping("/movies/{movieId}")
+  public ResponseEntity<List<Movies>> getMovieById(@PathVariable int movieId) {
+    List<Movies> moviesById = moviesService.getMovieById(movieId);
+    return moviesById.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(moviesById);
+  }
 }
